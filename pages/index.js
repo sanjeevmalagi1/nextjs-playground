6b8170ff24046f356140657f52e82298a0d198be
Sanjeev M <sanjeev.malagi@scripbox.com>
2020-12-19 17:22:33 +0530

    Remove existaing info from home page

--- pages/index.js
+++ pages/index.js
@@ -9,47 +9,12 @@ const Home = (props) => {
   return (
     <Layout>
       <Head>
-        <title>Create Next App</title>
+        <title>Price Of BTC</title>
       </Head>
       <main className={styles.main}>
         <h1>
           1 Bitcoin = {props.bpi.USD.code} {props.bpi.USD.rate} = {props.bpi.EUR.code} {props.bpi.EUR.rate}
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
